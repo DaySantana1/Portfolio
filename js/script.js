aeb6f835cdd6b21634a37105c244b8ab1dc8169d
Dayane Santana <dayane.santana@dedalus.eu>
2024-05-21 12:19:10 -0300

    projects

--- js/script.js
+++ js/script.js
@@ -407,5 +407,32 @@ let options = {
     }
   });
   
-
-  
+  (function($) {
+    "use strict";
+    
+    $(".bar").each(function() {
+      
+      var $bar = $(this),
+          $pct = $bar.find(".pct"),
+          data = $bar.data("bar");
+      
+      setTimeout(function() {
+        
+        $bar
+          .css("background-color", data.color)
+          .animate({
+            "width": $pct.html()
+        }, data.speed || 2000, function() {
+          
+          $pct.css({
+            "color": data.color,
+            "opacity": 1
+          });
+          
+        });
+        
+      }, data.delay || 0);
+      
+    });
+    
+  })(jQuery);
